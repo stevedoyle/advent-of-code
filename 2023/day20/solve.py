c4a296c6c044c30145b2c294b3834079455a120b
Stephen Doyle <stephendoyle75@gmail.com>
2024-04-27 01:31:56 +0100

    fix: Fix linter errors

--- 2023/day20/solve.py
+++ 2023/day20/solve.py
@@ -1,35 +1,65 @@
 # Solution from: https://www.reddit.com/r/adventofcode/comments/18mmfxb/comment/ke6c9ge/
 #
 with open("input.txt", "r") as file:
-    broadcasters, conjunctions, flipflops, relevant, queue, low, high, button, p2 = {}, {}, {}, {}, {"rx"}, 0, 0, 0, 1
+    broadcasters, conjunctions, flipflops, relevant, queue, low, high, button, p2 = (
+        {},
+        {},
+        {},
+        {},
+        {"rx"},
+        0,
+        0,
+        0,
+        1,
+    )
     for broadcaster, receivers in [x.split(" -> ") for x in file.read().splitlines()]:
         b, receivers = broadcaster[1:], receivers.split(", ")
-        if broadcaster == "broadcaster":  broadcasters[broadcaster] = {x : (lambda y: y) for x in receivers}
-        elif broadcaster.startswith("&"): broadcasters[b] = {x : (lambda y, b=b: set(conjunctions[b].values()) != {1}) for x in receivers}; conjunctions[b] = {}
-        elif broadcaster.startswith("%"): broadcasters[b] = {x : (lambda y, b=b: flipflops[b])                         for x in receivers}; flipflops[b] = 0
+        if broadcaster == "broadcaster":
+            broadcasters[broadcaster] = {x: (lambda y: y) for x in receivers}
+        elif broadcaster.startswith("&"):
+            broadcasters[b] = {
+                x: (lambda y, b=b: set(conjunctions[b].values()) != {1})
+                for x in receivers
+            }
+            conjunctions[b] = {}
+        elif broadcaster.startswith("%"):
+            broadcasters[b] = {x: (lambda y, b=b: flipflops[b]) for x in receivers}
+            flipflops[b] = 0
     for k, v in broadcasters.items():
         for x in v:
-            if x in conjunctions: conjunctions[x][k] = 0
+            if x in conjunctions:
+                conjunctions[x][k] = 0
     while queue:
         current = queue.pop()
         relevant[current] = nxt = [k for k, v in broadcasters.items() if current in v]
         for x in nxt:
-            if x not in relevant: queue.add(x)
+            if x not in relevant:
+                queue.add(x)
     conj_patterns = {x for x in set(sum(relevant.values(), [])) if x in conjunctions}
     while conj_patterns:
-        button += 1; low += 1
-        queue = [{"broadcaster" : (0, "button")}]
+        button += 1
+        low += 1
+        queue = [{"broadcaster": (0, "button")}]
         while queue:
             receiver, (signal, origin) = queue.pop(0).popitem()
-            if receiver not in broadcasters: continue
-            if receiver in conjunctions: conjunctions[receiver][origin] = signal
+            if receiver not in broadcasters:
+                continue
+            if receiver in conjunctions:
+                conjunctions[receiver][origin] = signal
             if receiver in flipflops:
-                if signal: continue
-                else: flipflops[receiver] = not flipflops[receiver]
+                if signal:
+                    continue
+                else:
+                    flipflops[receiver] = not flipflops[receiver]
             for remote, signal_func in broadcasters[receiver].items():
-                queue.append({remote : (sent := signal_func(signal), receiver)})
-                if sent == 1: high += 1
-                else:         low += 1
-            if receiver in conj_patterns and sent: conj_patterns.remove(receiver); p2 *= button
-        if button == 1000: p1 = low * high
+                queue.append({remote: (sent := signal_func(signal), receiver)})
+                if sent == 1:
+                    high += 1
+                else:
+                    low += 1
+            if receiver in conj_patterns and sent:
+                conj_patterns.remove(receiver)
+                p2 *= button
+        if button == 1000:
+            p1 = low * high
     print(p1, p2)
--- 2023/day21/solve.py
+++ 2023/day21/solve.py
@@ -5,20 +5,32 @@ from heapq import heapify, heappush, heappop
 
 with open("input.txt", "r") as file:
     data = file.read().splitlines()
-    ln, steps, cycle, seen, even, odd, n = len(data), 0, [], set(), set(), set(), 26501365
-    grid = {(x, y) : data[y][x] for x in range(ln) for y in range(ln)}
+    ln, steps, cycle, seen, even, odd, n = (
+        len(data),
+        0,
+        [],
+        set(),
+        set(),
+        set(),
+        26501365,
+    )
+    grid = {(x, y): data[y][x] for x in range(ln) for y in range(ln)}
     heapify(queue := [(steps, next((k for k, v in grid.items() if v == "S")))])
     while queue:
         new_steps, (x, y) = heappop(queue)
-        if (x, y) in seen: 
+        if (x, y) in seen:
             continue
         seen.add((x, y))
         if new_steps != steps:
-            if steps == 64: 
+            if steps == 64:
                 p1 = len(even)
             if steps % (ln * 2) == n % (ln * 2):
                 if len(cycle := cycle + [len([even, odd][steps % 2])]) == 3:
-                    p2, offset, increment = cycle[0], cycle[1] - cycle[0],  (cycle[2] - cycle[1]) - (cycle[1] - cycle[0])
+                    p2, offset, increment = (
+                        cycle[0],
+                        cycle[1] - cycle[0],
+                        (cycle[2] - cycle[1]) - (cycle[1] - cycle[0]),
+                    )
                     for x in range(n // (ln * 2)):
                         p2 += offset
                         offset += increment
