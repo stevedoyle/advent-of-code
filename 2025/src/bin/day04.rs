d74ecd28785f78f1b23618dc2e45d5bd4e1cfe17
Stephen Doyle <stephendoyle75@gmail.com>
2025-12-04 11:02:24 +0000

    Add 2025 Day04
    Clean up and simplify the solution.

--- 2025/src/bin/day04.rs
+++ 2025/src/bin/day04.rs
@@ -1,17 +1,47 @@
 use aoc2025::*;
 
-fn parse_input(input: &str) -> Vec<i32> {
-    parse_lines(input)
+fn parse_input(input: &str) -> Grid<char> {
+    parse_grid(input)
 }
 
-fn solve_p1(input: &str) -> i32 {
-    let _data = parse_input(input);
-    0
+fn find_moveable_tps(grid: &Grid<char>) -> Vec<(usize, usize)> {
+    grid::positions(grid)
+        .filter(|&(r, c)| grid[r][c] == '@')
+        .filter(|&(r, c)| {
+            grid::valid_neighbors8(grid, r as isize, c as isize)
+                .iter()
+                .filter(|(nr, nc)| grid[*nr][*nc] == '@')
+                .count()
+                < 4
+        })
+        .collect()
+}
+
+fn remove_tps(grid: &mut Grid<char>, tps: &[(usize, usize)]) {
+    for &(r, c) in tps {
+        grid[r][c] = '.';
+    }
+}
+
+fn solve_p1(input: &str) -> usize {
+    let grid = parse_input(input);
+    find_moveable_tps(&grid).len()
 }
 
 fn solve_p2(input: &str) -> i32 {
-    let _data = parse_input(input);
-    0
+    let mut grid = parse_input(input);
+    let mut total = 0;
+    let mut removed_some = true;
+    while removed_some {
+        let moveable_tps = find_moveable_tps(&grid);
+        if moveable_tps.is_empty() {
+            removed_some = false;
+        } else {
+            total += moveable_tps.len() as i32;
+            remove_tps(&mut grid, &moveable_tps);
+        }
+    }
+    total
 }
 
 fn main() {
@@ -36,8 +66,8 @@ mod tests {
     fn test_solve_with_test_input() {
         let input = read_test_input(4);
         let answer = solve_p1(&input);
-        assert_eq!(answer, 0);
+        assert_eq!(answer, 13);
         let answer = solve_p2(&input);
-        assert_eq!(answer, 0);
+        assert_eq!(answer, 43);
     }
 }
