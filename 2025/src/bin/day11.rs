967e882932057074a4d88de0df78993f1a33e293
Stephen Doyle <stephendoyle75@gmail.com>
2025-12-11 10:48:35 +0000

    Add 2025 Day11

--- 2025/src/bin/day11.rs
+++ 2025/src/bin/day11.rs
@@ -1,17 +1,57 @@
+use pathfinding::prelude::count_paths;
+use std::collections::HashMap;
+
 use aoc2025::*;
 
-fn parse_input(input: &str) -> Vec<i32> {
-    parse_lines(input)
+fn parse_input(input: &str) -> HashMap<String, Vec<String>> {
+    let mut data = HashMap::new();
+
+    for line in input.lines() {
+        let parts: Vec<&str> = line.split(": ").map(|s| s.trim()).collect();
+        if parts.len() == 2 {
+            let key = parts[0].to_string();
+            let values: Vec<String> = parts[1].split(' ').map(|s| s.trim().to_string()).collect();
+            data.insert(key, values);
+        }
+    }
+
+    data
 }
 
-fn solve_p1(input: &str) -> i32 {
-    let _data = parse_input(input);
-    0
+fn solve_p1(input: &str) -> usize {
+    let devices = parse_input(input);
+    let start = "you".to_string();
+    let target = "out".to_string();
+
+    count_paths(
+        &start,
+        |from| devices.get(*from).unwrap(),
+        |node| **node == target,
+    )
 }
 
-fn solve_p2(input: &str) -> i32 {
-    let _data = parse_input(input);
-    0
+fn solve_p2(input: &str) -> usize {
+    let mut devices = parse_input(input);
+    let start = "svr".to_string();
+    let target = "out".to_string();
+    devices.insert(target.clone(), vec![]); // Ensure target is in the map
+
+    count_paths(
+        (&start, false, false),
+        |from| {
+            let (current, has_dac, has_fft) = *from;
+            devices.get(current).unwrap().iter().map(move |neighbor| {
+                if neighbor == "dac" {
+                    (neighbor, true, has_fft)
+                } else if neighbor == "fft" {
+                    (neighbor, has_dac, true)
+                } else {
+                    (neighbor, has_dac, has_fft)
+                }
+            })
+        },
+        |node| **node.0 == target && node.1 && node.2,
+    )
 }
 
 fn main() {
@@ -36,8 +76,10 @@ mod tests {
     fn test_solve_with_test_input() {
         let input = read_test_input(11);
         let answer = solve_p1(&input);
-        assert_eq!(answer, 0);
+        assert_eq!(answer, 5);
+
+        let input = read_test_input_2(11);
         let answer = solve_p2(&input);
-        assert_eq!(answer, 0);
+        assert_eq!(answer, 2);
     }
 }
